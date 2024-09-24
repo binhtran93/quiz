b7ec427b25c25469bf1e5eda7a8f07b8408508e5
binh.tran <b.tran@inpsyde.com>
2024-09-24 12:45:15 +0700

    Format code

--- backend/src/quiz/controllers/quiz-controller.ts
+++ backend/src/quiz/controllers/quiz-controller.ts
@@ -2,14 +2,12 @@ import { Request, Response } from 'express'
 import { injectable } from 'inversify'
 import GetQuizzes from '../services/get-quizzes'
 import GetQuestions from '../services/get-questions'
-import SubmitAnswer from '../services/submit-answer'
 
 @injectable()
 export default class QuizController {
   constructor(
     private getQuizzes: GetQuizzes,
     private getQuestions: GetQuestions,
-    private submitAnswerServ: SubmitAnswer
   ) {}
 
   public async getAll(req: Request, res: Response) {
