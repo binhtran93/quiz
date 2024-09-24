c4e6569f9f76e7f4fadc027b92e8eca3260136d9
binh.tran <b.tran@inpsyde.com>
2024-09-25 00:17:19 +0700

    udpate readme

--- backend/src/redis/client.ts
+++ backend/src/redis/client.ts
@@ -3,14 +3,6 @@ const client = createClient({
   url: 'redis://redis:6379'
 })
 
-// Connect to Redis
 client.connect()
-client.on('connect', () => {
-  console.log('Connected to Redis')
-})
-
-client.on('error', (err) => {
-  console.error('Redis error:', err)
-})
 
 export default client
